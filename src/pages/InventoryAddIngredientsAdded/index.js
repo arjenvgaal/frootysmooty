9188be23fa7d1385fdf9f6a25d0c948b16c6eb31
Arjen van Gaal <arjen.van.gaal@goin.team>
2019-09-07 23:23:28 +0200

    gifjes

--- src/pages/InventoryAddIngredientsAdded/index.js
+++ src/pages/InventoryAddIngredientsAdded/index.js
@@ -3,7 +3,7 @@ import PageBackground from '../../components/PageBackground';
 import StyledButton from '../../components/StyledButton';
 import screen from'../../assets/screen.inventory.add-ingredients.added.png';
 import { withNavigation } from 'react-navigation';
-import {StyleSheet} from "react-native";
+import {StyleSheet, Image, View} from "react-native";
 
 const primaryButtonStyles = StyleSheet.create({
   position: {
@@ -18,11 +18,29 @@ const primaryButtonStyles = StyleSheet.create({
   }
 });
 
+const styles = StyleSheet.create({
+  bigGifStyle: {
+    width: 275 * 1.3,    
+    height: 154 * 1.3,    
+  },
+  gifContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    position: 'absolute',
+    width: "100%",
+    top: 120,
+  }
+});
 
 const InventoryAddIngredientsAdded = ({ navigation }) => {
 
   return (
     <PageBackground source={screen}>
+      <View style={styles.gifContainer}>
+        <Image source={require('../../assets/images/apple.gif')} resizeMode="cover" style={styles.bigGifStyle} />
+      </View>
+
       <StyledButton
         style={[primaryButtonStyles.position, primaryButtonStyles.size]}
         onPress={() => navigation.replace('Inventory')}
--- src/pages/SmoothiesSmoothieComplete/index.js
+++ src/pages/SmoothiesSmoothieComplete/index.js
@@ -3,7 +3,7 @@ import PageBackground from '../../components/PageBackground';
 import StyledButton from '../../components/StyledButton';
 import screen from'../../assets/screen.smoothies.smoothie-complete.png';
 import { withNavigation } from 'react-navigation';
-import {StyleSheet} from "react-native";
+import {StyleSheet, Image, View} from "react-native";
 
 const primaryButtonStyles = StyleSheet.create({
   position: {
@@ -14,14 +14,35 @@ const primaryButtonStyles = StyleSheet.create({
   size: {
     width: 50,
     height: 50,
-    backgroundColor: 'rgba(52, 52, 52, 0.8)',
+    // backgroundColor: 'rgba(52, 52, 52, 0.8)',
   }
 });
+
+
+const styles = StyleSheet.create({
+  bigGifStyle: {
+    width: 275 * 1.3,    
+    height: 154 * 1.3,    
+  },
+  gifContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    position: 'absolute',
+    width: "100%",
+    top: 120,
+  }
+});
+
 
 
 const SmoothiesSmoothieComplete = ({ navigation }) => {
   return (
     <PageBackground source={screen}>
+      <View style={styles.gifContainer}>
+        <Image source={require('../../assets/images/broc.gif')} resizeMode="cover" style={styles.bigGifStyle} />
+      </View>
+
       <StyledButton
         style={[primaryButtonStyles.position, primaryButtonStyles.size]}
         onPress={() => navigation.navigate('Smoothies')}
