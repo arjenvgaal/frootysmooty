7f022a3063942743b0bf67e63c23294105c2fe34
Arjen van Gaal <arjen.van.gaal@goin.team>
2019-09-08 10:23:33 +0200

    youtube

--- src/pages/SmoothiesSmoothie/index.js
+++ src/pages/SmoothiesSmoothie/index.js
@@ -3,8 +3,9 @@ import PageBackground from '../../components/PageBackground';
 import StyledButton from '../../components/StyledButton';
 import screen from'../../assets/screen.smoothies.smoothie.png';
 import { withNavigation } from 'react-navigation';
-import {StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
 import BackButton from "../../components/BackButton";
+import YouTube from 'react-native-youtube';
 
 const primaryButtonStyles = StyleSheet.create({
   position: {
@@ -36,6 +37,22 @@ const SmoothiesSmoothie = ({ navigation }) => {
   return (
     <PageBackground source={screen}>
       <BackButton onPress={() => navigation.navigate('Smoothies')} />
+
+    <View style={{alignSelf: "center", marginTop: 150}}>
+        <YouTube
+          apiKey="AIzaSyCApm0j3T1VLEGthWk1ejG01hTdm-mU7oc"
+          videoId="_0Xy0RiIN5w"   // The YouTube video ID
+          play={true}             // control playback of video with true/false
+          fullscreen={false}       // control whether the video should play in fullscreen or inline
+          loop={false}             // control whether the video should loop when ended
+          // onReady={e => this.setState({ isReady: true })}
+          // onChangeState={e => this.setState({ status: e.state })}
+          // onChangeQuality={e => this.setState({ quality: e.quality })}
+          // onError={e => this.setState({ error: e.error })}
+          style={{ alignSelf: 'center', width: 285, height: 200 }}
+        />
+      </View>
+
 
       <StyledButton
         style={[primaryButtonStyles.position, primaryButtonStyles.size]}
